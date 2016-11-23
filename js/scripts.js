35b0a02a02ef071ca62dbd21497651b5701b8dc5
Ziggy Andzel <ziggy.andzel@gmail.comgit commit -m zmiana>
2016-11-23 13:07:12 +0100

    drugie zadanie dodane

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@
 var name = prompt('Podej swoje imię');
 alert('Witoj, ' + name);
 console.log('Witaj, ' + name);
+
+
+// pole trójkąta
+
+var a = prompt('Podaj długość podstawy');
+var h = prompt('Podaj wysokość');
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+alert('Pole trójkąta wynosi ' + triangleArea);
