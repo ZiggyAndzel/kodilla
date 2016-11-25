2ea66bede68edc32ebe75603118dee1927b68e4a
Ziggy Andzel <ziggy.andzel@gmail.comgit commit -m zmiana>
2016-11-25 16:14:09 +0100

    plik js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// zadanie 8_5
+// Funkcja pole trójkąta
 
-var a = prompt('podaj pierwszą liczbę');
-var b = prompt('podaj drugą liczbę');
-var value = (a*a) + (2*a*b) - (b*b);
+var getTriangleValue1 = getTriangleValue(5,5);
+var getTriangleValue2 = getTriangleValue(7,5);
+var getTriangleValue3 = getTriangleValue(5,-5);
 
-if (value == 0) {
-	console.log('Wynik równa się zero');
-} else if (value > 0) {
-		console.log('Wynik to ' + value + ' i jest większy od zera');
-} else {
-	console.log('Wynik to ' + value + ' i jest mniejszy od zera');
-}
+
+function getTriangleValue(a, h) {
+
+	if (a <= 0 || h <= 0) {
+		return 'Fatal error. Autodestruction sequence initiated.';
+	} else {
+		return a*h/2;
+	}
+};
+
+console.log(getTriangleValue1, getTriangleValue2, getTriangleValue3);
